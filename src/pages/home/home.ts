bb830f9d43ca6892f6ff44f17acd2c3c43591e4a
Sebastian <ingesql1992@hotmail.com>
2018-09-12 06:26:21 -0500

    Obserbable fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,7 +5,7 @@ import { GlobalProvider } from '../../providers/global/global';
 import { Geolocation } from '@ionic-native/geolocation';
 import { BatteryStatus } from '@ionic-native/battery-status';
 import { Device } from '@ionic-native/device';
-import { Observable } from 'Rxjs/Rx';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'page-home',
